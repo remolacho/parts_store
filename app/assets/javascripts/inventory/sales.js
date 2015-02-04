a6e77caaede43cc8da7ccd88ef66380fe10eeece
jonathan <jonathang_25@hotmail.com>
2015-02-04 00:51:37 -0430

    se esta agregando el modulo de ventas

--- app/assets/javascripts/inventory/sales.js
+++ app/assets/javascripts/inventory/sales.js
@@ -1,15 +1,18 @@
 $(function () {
-   
+   var existenceinicial = parseInt($('#txtexistence').val())
    $("#txtquantity").keyup(function(event){
 
    	  if ($('#txtquantity').val() == "" || $('#txtquantity').val() == null ) {
-        $('#txtamount').val("0");    
+        $('#txtamount').val("0");
+        $('#txtexistence').val(existenceinicial);
    	  }else{
         amount   = 0;
         quantity = parseInt($('#txtquantity').val());
         price    = parseInt($('#txtprice').val());
         amount = price * quantity
-        $('#txtamount').val(amount)   	  	
+        existence = existenceinicial - quantity
+        $('#txtamount').val(amount)   	
+        $('#txtexistence').val(existence)  	
    	  }
 
    });
